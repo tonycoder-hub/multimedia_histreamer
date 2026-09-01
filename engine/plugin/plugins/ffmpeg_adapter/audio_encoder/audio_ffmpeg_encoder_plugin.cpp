95256e626768ca9ece02bb9bdaebe15d8594d2b6
Tony Coder <407243179@qq.com>
2026-09-01 20:02:25 +0000

    fix: leftover FillInFrameCache srcBytesPerSample_==0 SIGFPE
    Skip destLength / srcBytesPerSample_ when the divisor is 0 so
    FillInFrameCache does not SIGFPE on a malformed encoder context.
    
    Signed-off-by: Tony Coder <407243179@qq.com>

--- engine/plugin/plugins/ffmpeg_adapter/audio_encoder/audio_ffmpeg_encoder_plugin.cpp
+++ engine/plugin/plugins/ffmpeg_adapter/audio_encoder/audio_ffmpeg_encoder_plugin.cpp
@@ -380,7 +380,12 @@ void AudioFfmpegEncoderPlugin::FillInFrameCache(const std::shared_ptr<Memory>& m
         }
     } else {
         sampleData = destBuffer;
-        nbSamples = destLength / srcBytesPerSample_;
+        uint32_t srcBytesPerSample = srcBytesPerSample_;
+        if (srcBytesPerSample) {
+            nbSamples = destLength / srcBytesPerSample;
+        } else {
+            MEDIA_LOG_E("FillInFrameCache divided by zero");
+        }
     }
     cachedFrame_->format = avCodecContext_->sample_fmt;
     cachedFrame_->sample_rate = avCodecContext_->sample_rate;
